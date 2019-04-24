42eba39ccf0d35085914954593150aad5f88a267
Orivaldo Santana Jr <orivaldo@gmail.com>
2019-04-24 13:37:40 -0300

    Create etapa1.js

---
+++ etapa1.js
@@ -0,0 +1,22 @@
+/* 
+    Equipe: 
+        Nome Completo do Aluno I - Subturma X (Líder) 
+        Nome Completo do Aluno II - Subturma Z 
+        Etapa 1
+*/
+
+
+// https://editor.p5js.org/
+
+var xi = 200;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(100);
+  rect(xi, 20, 55, 55);
+
+  ellipse(200, 350, 30, 50);
+}
